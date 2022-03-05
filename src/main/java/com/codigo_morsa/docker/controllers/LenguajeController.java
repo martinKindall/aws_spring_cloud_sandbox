40f40d241282c8c6d7660428874f91bcb93944dc
Martin <mart256@gmail.com>
2022-03-05 20:15:19 +0100

    Added post route for saving lenguajes

--- src/main/java/com/codigo_morsa/docker/controllers/LenguajeController.java
+++ src/main/java/com/codigo_morsa/docker/controllers/LenguajeController.java
@@ -3,11 +3,11 @@ package com.codigo_morsa.docker.controllers;
 import com.codigo_morsa.docker.model.Lenguaje;
 import com.codigo_morsa.docker.repository.LenguajeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 public class LenguajeController {
@@ -27,5 +27,10 @@ public class LenguajeController {
     @GetMapping("/lenguaje/{name}")
     public Mono<Lenguaje> getAllLenguajes(@PathVariable String name) {
         return repository.getLenguajeByName(name);
+    }
+
+    @PostMapping("/lenguaje")
+    public Mono<Map<String, String>> saveLenguaje(@RequestBody Lenguaje lenguaje) {
+        return repository.saveLenguaje(lenguaje).thenReturn(Map.of("message", "Lenguaje creado!"));
     }
 }
--- src/main/java/com/codigo_morsa/docker/repository/LenguajeRepository.java
+++ src/main/java/com/codigo_morsa/docker/repository/LenguajeRepository.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.*;
+import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
+
+import java.util.Map;
 
 @Repository
 public class LenguajeRepository {
@@ -27,6 +27,19 @@ public class LenguajeRepository {
 
     public Mono<Lenguaje> getLenguajeByName(String name) {
         return Mono.fromFuture(lenguajeTable.getItem(getKeyBuild(name)));
+    }
+
+    public Mono<Void> saveLenguaje(Lenguaje lenguaje) {
+        var request = PutItemEnhancedRequest
+                .builder(Lenguaje.class)
+                .item(lenguaje)
+                .conditionExpression(
+                        Expression.builder()
+                                .expression("attribute_not_exists(#name)")
+                                .expressionNames(Map.of("#name", "name"))
+                                .build())
+                .build();
+        return Mono.fromFuture(lenguajeTable.putItem(request));
     }
 
     private Key getKeyBuild(String name) {
