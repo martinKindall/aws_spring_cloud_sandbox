4830dc8d95a9a9f06b2c82fdb699fa2ec0ea3e56
Martin <mart256@gmail.com>
2022-04-18 18:24:32 +0200

    Login to Docker before building image. Added error handling to script - version

--- build_image.sh
+++ build_image.sh
@@ -1,11 +1,36 @@
+#!/bin/sh
+
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
+
 sh ./gradlew bootJar
 mkdir build/dependency
 cd build/dependency
 jar -xf ../libs/*.jar
 cd ../..
-docker build -t springio/dockerize-spring-boot .
-docker tag springio/dockerize-spring-boot:latest public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f5n7q8r5
 
+docker build -t springio/dockerize-spring-boot .
+docker tag springio/dockerize-spring-boot:latest public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
 docker push public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
+
+trap : 0
+
+echo >&2 '
+************
+*** DONE ***
+************
+'
