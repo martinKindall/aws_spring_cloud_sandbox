88dfdc260d626c6c74271665dd7bb1acd9e16339
Martin <mart256@gmail.com>
2022-04-18 17:52:46 +0200

    Added a buildspec.yaml for CodeBuild. Added login step - version

--- build_image.sh
+++ build_image.sh
@@ -4,4 +4,7 @@ jar -xf ../libs/*.jar
 cd ../..
 docker build -t springio/dockerize-spring-boot .
 docker tag springio/dockerize-spring-boot:latest public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
+
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f5n7q8r5
+
 docker push public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
