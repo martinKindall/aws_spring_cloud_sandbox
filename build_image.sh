34b2550bbcd0671082e800e0ccfef54e52774f6e
Martin <mart256@gmail.com>
2022-04-21 21:03:20 +0200

    Building new image based on feature branch or main branch

--- build_image.sh
+++ build_image.sh
@@ -24,11 +24,17 @@ cd ../..
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 371417955885.dkr.ecr.us-east-1.amazonaws.com
 
 docker build -t springio/dockerize-spring-boot .
-docker tag springio/dockerize-spring-boot:latest public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
+
+# Extract branch name from CODEBUILD_WEBHOOK_HEAD_REF
+# E.g. refs/heads/main
+regex="([^\/]+$)"
+TAG=$([ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_MERGED" ] && [ "$CODEBUILD_WEBHOOK_BASE_REF" = "refs/heads/main" ] && echo "latest" || if [[ $CODEBUILD_WEBHOOK_HEAD_REF =~ $regex ]]; then echo ${BASH_REMATCH[1]}; fi)
+IMAGE_URI=public.ecr.aws/f5n7q8r5/aws-spring-param-store:$TAG
+docker tag springio/dockerize-spring-boot:latest IMAGE_URI
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f5n7q8r5
 
-docker push public.ecr.aws/f5n7q8r5/aws-spring-param-store:latest
+docker push IMAGE_URI
 
 trap : 0
 
