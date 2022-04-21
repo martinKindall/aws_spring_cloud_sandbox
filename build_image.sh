4a60e82be527e5f76c07de44cf44bf26f94b64fe
Martin <mart256@gmail.com>
2022-04-21 21:14:42 +0200

    Checking if environment vars are inherited

--- build_image.sh
+++ build_image.sh
@@ -28,8 +28,12 @@ docker build -t springio/dockerize-spring-boot .
 # Extract branch name from CODEBUILD_WEBHOOK_HEAD_REF
 # E.g. refs/heads/main
 regex="([^\/]+$)"
+echo $CODEBUILD_WEBHOOK_EVENT
+echo $CODEBUILD_WEBHOOK_BASE_REF
+echo $CODEBUILD_WEBHOOK_HEAD_REF
 TAG=$([ "$CODEBUILD_WEBHOOK_EVENT" = "PULL_REQUEST_MERGED" ] && [ "$CODEBUILD_WEBHOOK_BASE_REF" = "refs/heads/main" ] && echo "latest" || if [[ $CODEBUILD_WEBHOOK_HEAD_REF =~ $regex ]]; then echo ${BASH_REMATCH[1]}; fi)
 IMAGE_URI=public.ecr.aws/f5n7q8r5/aws-spring-param-store:$TAG
+echo "the tag $TAG, $IMAGE_URI"
 docker tag springio/dockerize-spring-boot:latest $IMAGE_URI
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f5n7q8r5
